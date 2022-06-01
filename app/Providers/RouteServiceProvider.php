e3073a0295681ee8e58ec7b7414863e537b45cf2
aqiltalent <aqil.salahov13@gmail.com>
2022-06-02 00:26:16 +0200

    Clear basic configuration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -25,7 +25,8 @@ class RouteServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	: void
+	{
         $this->configureRateLimiting();
 
         $this->routes(function () {
@@ -44,7 +45,8 @@ class RouteServiceProvider extends ServiceProvider
      * @return void
      */
     protected function configureRateLimiting()
-    {
+	: void
+	{
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
