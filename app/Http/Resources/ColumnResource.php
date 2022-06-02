7c68fd71f4a5f9ff1f85ae3eb78b252ce173e4d3
aqiltalent <aqil.salahov13@gmail.com>
2022-06-02 02:10:19 +0200

    updated column resource collection

--- app/Http/Resources/ColumnResource.php
+++ app/Http/Resources/ColumnResource.php
@@ -21,13 +21,24 @@ class ColumnResource extends JsonResource
 		return [
 			'id'    => $this->id,
 			'title' => $this->column_title,
-			'cards' => $this->cards->map(function ($el) {
+			'cards' => $this->cards->map(function ($card) {
 				return [
-					'card_id'          => $el->id,
-					'card_title'       => $el->card_title,
-					'card_description' => $el->card_description,
-					'card_order'       => $el->card_order,
-					'comments'         => $el->comments()->orderBy('created_at')->get(),
+					'id'               => $card->id,
+					'column_id'        => $card->column_id,
+					'card_title'       => $card->card_title,
+					'card_description' => $card->card_description,
+					'card_order'       => $card->card_order,
+					'comments'         => $card->comments()
+						->orderBy('created_at')
+						->get()
+						->map(function ($comment) {
+							return [
+								'id'          => $comment->id,
+								'card_id'     => $comment->card_id,
+								'description' => $comment->description,
+								'created_at'  => $comment->created_at,
+							];
+						}),
 				];
 			}),
 		];
